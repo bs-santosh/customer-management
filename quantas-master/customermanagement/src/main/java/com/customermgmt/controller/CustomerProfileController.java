12fc2e080b676fdbb29a054ba6634e77716ad96f
bs.santosha@gmail.com <bs.santosha@gmail.com>
2019-03-05 23:10:05 +1100

    Add basic API documentation using Swagger annotations

--- quantas-master/customermanagement/src/main/java/com/customermgmt/controller/CustomerProfileController.java
+++ quantas-master/customermanagement/src/main/java/com/customermgmt/controller/CustomerProfileController.java
@@ -3,6 +3,8 @@ package com.customermgmt.controller;
 import com.customermgmt.dto.CustomerProfileDto;
 import com.customermgmt.dto.CustomerProfileResponse;
 import com.customermgmt.service.CustomerProfileService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,10 @@ public class CustomerProfileController {
     @Autowired
     CustomerProfileService customerProfileService;
 
+    @ApiOperation(value = "Find all customers",
+            notes = "Returns a JSON string of all the available customer profiles",
+            response = CustomerProfileResponse.class,
+            responseContainer = "List")
     @GetMapping(value = "/customers/find/all")
     public CustomerProfileResponse findAllCustomers() {
 
@@ -22,12 +28,17 @@ public class CustomerProfileController {
                 .build();
     }
 
+    @ApiOperation(value = "Save a Customer Profile",
+            notes = "Saves customer profile received as JSON post")
     @PostMapping(value = "/customers/save")
     public void saveCustomerProfile(@RequestBody CustomerProfileDto customerProfileDto) {
         customerProfileService.saveCustomerProfile(customerProfileDto);
     }
 
-
+    @ApiOperation(value = "Find a single customer profile",
+            notes = "Returns a JSON string of a customer profile based on Customer ID",
+            response = CustomerProfileResponse.class,
+            responseContainer = "List")
     @GetMapping(value="/customers/find/{customerId}")
     public CustomerProfileResponse findSingleCustomer(@PathVariable(name = "customerId") Long customerId) {
         System.out.println("customerId = " + customerId);
@@ -39,6 +50,8 @@ public class CustomerProfileController {
                 .build();
     }
 
+    @ApiOperation(value = "Deletes a single customer profile",
+            notes = "Deletes a single customer profile based on ID")
     @PostMapping(value = "/customers/delete")
     public void deleteCustomerProfile(@RequestBody CustomerProfileDto customerProfileDto) {
         customerProfileService.deleteCustomerProfile(customerProfileDto.getId());
